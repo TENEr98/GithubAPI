1b299f6f43adfbf7d634a50a1a1866fdb2b8fe58
TENEr98 <levon.tarayan.98@mail.ru>
2021-09-13 18:18:11 +0500

    finished app

--- index.js
+++ index.js
@@ -15,14 +15,50 @@ const debounce = (fn, ms) => {
   };
 };
 
+const createDOMElement = (tag, params) => {
+  const newElement = document.createElement(tag);
+  Object.keys(params).forEach((key) => {
+    switch (key) {
+      case "classList": {
+        params.classList.forEach((className) =>
+          newElement.classList.add(className)
+        );
+        break;
+      }
+      case "for":
+      case "style": {
+        newElement.setAttribute(key, params[key]);
+        break;
+      }
+      case "children": {
+        for (let i = 0; i < params.children.length; i++) {
+          newElement.appendChild(params.children[i]);
+        }
+        break;
+      }
+      default: {
+        newElement[key] = params[key];
+        break;
+      }
+    }
+  });
+  return newElement;
+};
+
 const onSearchButton = () => {
   error.innerHTML = "";
+  clearAutocomplete();
+};
+
+const clearAutocomplete = () => {
+  autocompleteContainer.innerHTML = "";
+  autocompleteContainer.classList.remove("active");
 };
 
 const onChange = (event) => {
   if (event.target.value.length === 0) {
     error.innerHTML = "";
-    autocompleteContainer.classList.remove("active");
+    clearAutocomplete();
   }
   fetch(
     "https://api.github.com/search/repositories?per_page=5&q=" +
@@ -34,6 +70,7 @@ const onChange = (event) => {
       if (Array.isArray(items) && items.length === 0) {
         error.style.display = "block";
         error.innerHTML = "Нет такого репозитория";
+        clearAutocomplete();
       } else {
         createAutocompleteList(items);
       }
@@ -43,16 +80,78 @@ const onChange = (event) => {
     });
 };
 
-const createSavedList = (event) => {
-  const savedItem = document.createElement("div");
-  savedItem.innerHTML = el.name;
-  savedItem.classList.add("list-item");
-  listContainer.appendChild(savedItem);
+const removeListItem = (itemId) => {
+  savedList.splice(itemId, 1);
+  createSavedList();
+};
+
+const createSavedList = () => {
+  inputSearch.value = "";
+  clearAutocomplete();
+  listContainer.innerHTML = "";
+  savedList.forEach((el, idx) => {
+    const savedItem = createDOMElement("div", {
+      classList: ["list-item"],
+      children: [
+        createDOMElement("div", {
+          classList: ["item-props"],
+          children: [
+            createDOMElement("div", {
+              classList: ["item-name"],
+              children: [
+                createDOMElement("span", {
+                  innerText: "Название",
+                }),
+                createDOMElement("span", {
+                  innerText: el.name,
+                }),
+              ],
+            }),
+            createDOMElement("div", {
+              classList: ["item-owner"],
+              children: [
+                createDOMElement("span", {
+                  innerText: "Владелец",
+                }),
+                createDOMElement("span", {
+                  innerText: el.login,
+                }),
+              ],
+            }),
+            createDOMElement("div", {
+              classList: ["item-stars"],
+              children: [
+                createDOMElement("span", {
+                  innerText: "Звезд",
+                }),
+                createDOMElement("span", {
+                  innerText: el.stargazers_count,
+                }),
+              ],
+            }),
+          ],
+        }),
+        createDOMElement("div", {
+          classList: ["item-trash"],
+          onclick: () => removeListItem(idx),
+          children: [
+            createDOMElement("img", {
+              classList: ["trash"],
+              src: "./trash.svg",
+              alt: "урна",
+            }),
+          ],
+        }),
+      ],
+    });
+    listContainer.appendChild(savedItem);
+  });
 };
 
 const createAutocompleteList = (items) => {
-  autocompleteContainer.innerHTML = "";
+  clearAutocomplete();
   searchList.length = 0;
+  console.log(searchList);
   items.forEach((item) => {
     const {
       name,
@@ -74,8 +173,18 @@ const createAutocompleteList = (items) => {
     autocompleteItem.innerHTML = el.name;
     autocompleteItem.classList.add("auto-item");
     autocompleteItem.setAttribute("tabindex", "0");
-    // autocompleteItem.addEventListener("click", (event) => {});
-    // autocompleteItem.addEventListener("keyup"(event));
+    autocompleteItem.addEventListener("click", (event) => {
+      if (event.type === "click") {
+        savedList.push(el);
+        createSavedList(event);
+      }
+    });
+    autocompleteItem.addEventListener("keyup", (event) => {
+      if (event.keyCode === 13) {
+        savedList.push(el);
+        createSavedList(event);
+      }
+    });
     autocompleteContainer.appendChild(autocompleteItem);
   });
 };
